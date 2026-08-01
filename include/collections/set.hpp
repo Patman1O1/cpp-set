f0f533a1feb5fdd39bfd74c2cd932702af1ef98c
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-31 21:18:00 -0500

    Edit set.hpp

--- include/collections/set.hpp
+++ include/collections/set.hpp
@@ -1,4 +1,17 @@
-#ifndef CPP_SET_HPP
-#define CPP_SET_HPP
+#ifndef COLLECTIONS_SET_HPP
+#define COLLECTIONS_SET_HPP
 
-#endif // #ifndef CPP_SET_HPP
+// ISO C Includes
+#include <cstddef>
+
+// ISO C++ Includes
+#include <ranges>
+
+namespace collections {
+    template<typename T, std::ranges::range Container>
+    class set {
+    
+    };
+} // namespace collections
+
+#endif // #ifndef COLLECTIONS_SET_HPP
